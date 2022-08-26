d736a2abc9664021ed5c562824461aa7c49b6c09
fede <fedeselah7@gmail.com>
2022-08-25 21:04:45 -0300

    merequetengue

--- catalogo.php
+++ catalogo.php
@@ -1,34 +1,36 @@
 <?php include('header.php') ?>
+<br> <br> 
+<div class="main-wrapper" style="transform: scale(1);">
+    <div class="main-wrapper_container">
+        <div class="front_page_cat">
 
-<div class="main-wrapper">
-    <div class="front_page_cat">
+            <h1 class="cat_title">Catálogo</h1>
 
-    <h1 class="cat_title">Catálogo</h1>
+            <i class="fa-solid fa-magnifying-glass"><input type="text" placeholder="Search..." name="search" class="search"></i>
 
-    <i class="fa-solid fa-magnifying-glass"><input type="text" placeholder="Search..." name="search" class="search"></i>
-
-    </div>
-
-    <!--    CARDS    -->
-    <div class="cont_wrapper">
-
-        <div class="card" id="card1">
-            <div class="cont_card">
-               <h2 class="card_title">
-                    Something awesome
-                </h2>
-                <p class="card_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, architecto alias. Repellat enim cum, eaque fugit veritatis repudiandae ea eveniet.
-                </p>
-                <a href="#" class="button_card">purchase</a>
-            </div>
         </div>
 
+        <!--    CARDS    -->
+        <div class="cont_wrapper">
+
+            <div class="card" id="card1">
+                <div class="cont_card">
+                    <h2 class="card_title">
+                        Something awesome
+                    </h2>
+                    <p class="card_body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, architecto alias. Repellat enim cum, eaque fugit veritatis repudiandae ea eveniet.
+                    </p>
+                    <a href="#" class="button_card">purchase</a>
+                </div>
+            </div>
+
+        </div>
     </div>
 </div>
 
 
-    <script src="js/script.js"></script>
-    <script src="js/navbar.js"></script>
-    <script src="js/search.js"></script>
+<script src="js/script.js"></script>
+<script src="js/navbar.js"></script>
+<script src="js/search.js"></script>
 <?php include('footer.php') ?>
