dc9f330f3c6d729f71943aa804a3a468262b188c
fede <fedeselah7@gmail.com>
2022-08-25 22:03:30 -0300

    catalogo

--- catalogo.php
+++ catalogo.php
@@ -5,14 +5,38 @@
 
             <h1 class="cat_title">Catálogo</h1>
 
-            <i class="fa-solid fa-magnifying-glass"><input type="text" placeholder="Search..." name="search" class="search"></i>
+            <i class="fa-solid fa-magnifying-glass" id="lupa"></i><input type="text" placeholder="Search..." name="search" class="search">
 
         </div>
 
         <!--    CARDS    -->
         <div class="cont_wrapper">
 
-            <div class="card" id="card1">
+            <div class="card">
+                <div class="cont_card">
+                    <h2 class="card_title">
+                        Something awesome
+                    </h2>
+                    <p class="card_body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, architecto alias. Repellat enim cum, eaque fugit veritatis repudiandae ea eveniet.
+                    </p>
+                    <a href="#" class="button_card">purchase</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="cont_card">
+                    <h2 class="card_title">
+                        Something awesome
+                    </h2>
+                    <p class="card_body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, architecto alias. Repellat enim cum, eaque fugit veritatis repudiandae ea eveniet.
+                    </p>
+                    <a href="#" class="button_card">purchase</a>
+                </div>
+            </div>
+
+            <div class="card">
                 <div class="cont_card">
                     <h2 class="card_title">
                         Something awesome
