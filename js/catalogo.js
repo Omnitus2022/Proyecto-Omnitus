874859b92f1e234dc86972bafe92656629da4914
Flavio Taccarelli <flaviolocobb@gmail.com>
2022-10-27 02:12:20 -0300

    pura droga

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,16 +1,15 @@
 const catalogo = () => {
-  var n = 0;
   $(".button_card").click(function () {
     const cId = this.attributes["card-id"].value;
     const cNom = this.attributes["card-nom"].value;
     const cQty = $("#" + cId + " .cant_cat")[0].value;
-
-    $(".shoppingCart").load("./carrito.php", {
-      id: cId,
-      nombre: cNom,
-      cantidad: cQty,
-    });
-    n++;
+    if (cQty != 0) {
+      $(".shoppingCart").load("./carrito.php", {
+        id: cId,
+        nombre: cNom,
+        cantidad: cQty,
+      });
+    }
   });
 };
 $(document).ready(catalogo);
