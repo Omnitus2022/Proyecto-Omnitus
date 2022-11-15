f282f2f67f8f8041b1ffefda17b0c8963d359429
Flavio Taccarelli <flaviolocobb@gmail.com>
2022-11-15 12:25:00 -0300

    empty page

--- php/POST/gestionHuerta.php
+++ php/POST/gestionHuerta.php
@@ -1,5 +1,7 @@
 <?php
 $PATH = $_SERVER['DOCUMENT_ROOT'] . '/Proyecto-Omnitus/';
+require_once($PATH . 'php/views/empty.php');
+
 require($PATH . "php/db/db.php");
 require_once($PATH . 'php/models/huerta_model.php');
 require_once($PATH . 'php/models/table_model.php');
